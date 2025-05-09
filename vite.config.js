e83928efcb6d27fe90ab7eb39363971b7dd960bc
Joseph <gchoi325@my.yorku.ca>
2025-05-09 18:05:30 -0400

    VITE fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.VITE_DSBC || '/DSBC',
+  base: '/DSBC',
   plugins: [react()],
   server: {
     proxy: {
